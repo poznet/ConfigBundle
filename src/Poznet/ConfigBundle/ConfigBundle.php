9b16248f9eaccc7bd12a1619d270f69721f34a30
Michal Glajc <michal@glajc.pl>
2016-06-16 00:06:19 +0200

    registering DI in bundle

--- src/Poznet/ConfigBundle/ConfigBundle.php
+++ src/Poznet/ConfigBundle/ConfigBundle.php
@@ -3,7 +3,12 @@
 namespace Poznet\ConfigBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Poznet\ConfigBundle\DependencyInjection\PoznetConfigExtension;
 
 class ConfigBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new PoznetConfigExtension();
+    }
 }
