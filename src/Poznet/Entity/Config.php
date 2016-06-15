901f125f199e35e02f3aa16ebe9d0d8bab048f4b
Michal Glajc <michal@glajc.pl>
2016-06-15 22:55:39 +0200

    initial commit

---
+++ src/Poznet/Entity/Config.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace ConfigBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Config
+ *
+ * @ORM\Table(name="config")
+ * @ORM\Entity(repositoryClass="ConfigBundle\Repository\ConfigRepository")
+ */
+class Config
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @var
+     * @ORM\Column(name="name", type="string")
+     */
+    private $name;
+
+
+    /**
+     * @var
+     * @ORM\Column(name="value", type="string")
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Config
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return Config
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
---
+++ src/Poznet/Service/ConfigService.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joseph
+ * Date: 5/27/16
+ * Time: 9:16 AM
+ */
+
+namespace ConfigBundle\Service;
+
+
+use ConfigBundle\Entity\Config;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Klasa  zarządzająca konfiguracją
+ * Class ConfigService
+ * @package ConfigBundle\Service
+ */
+class ConfigService
+{
+
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em=$em;
+    }
+
+
+    /**
+     * Gets  config  value
+     * @param $name
+     * @return null
+     */
+    public function get($name){
+        $config=$this->em->getRepository("ConfigBundle:Config")->findOneByName($name);
+        if(!$config)
+            return null;
+        return $config->getValue();
+    }
+
+    /**
+     * Sets config value
+     * @param $name
+     * @param $value
+     * @return bool
+     */
+    public function set($name,$value){
+        $config=$this->em->getRepository("ConfigBundle:Config")->findOneByName($name);
+        if(!$config) {
+            $config = new Config();
+            $config->setName($name);
+            $config->setValue($value);
+            $this->em->persist($config);
+
+        }else{
+            $config->setName($name);
+            $config->setValue($value);
+        }
+        $this->em->flush();
+        return true;
+    }
+
+
+}
